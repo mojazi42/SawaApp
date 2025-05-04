3eb4dd36e77eccecd7f55ad95b999d6e4a761c4a
etharalrehaili <85852139+etharalrehaili@users.noreply.github.com>
2025-05-04 19:48:19 +0300

    shared preferences

--- app/src/main/java/com/example/sawaapplication/core/di/AppModule.kt
+++ app/src/main/java/com/example/sawaapplication/core/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.example.sawaapplication.core.di
 
+import com.example.sawaapplication.core.sharedPreferences.AuthInterceptor
+import com.example.sawaapplication.core.sharedPreferences.OkHTTPBuilder
+import com.example.sawaapplication.core.sharedPreferences.TokenProvider
 import com.example.sawaapplication.screens.authentication.data.remote.FirebaseAuthDataSource
 import com.example.sawaapplication.screens.authentication.data.repository.AuthRepositoryImpl
 import com.example.sawaapplication.screens.authentication.domain.repository.AuthRepository
@@ -8,6 +11,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -21,9 +25,25 @@ object AppModule {
         firebaseAuth: FirebaseAuth
     ): FirebaseAuthDataSource = FirebaseAuthDataSource(firebaseAuth)
 
-
     @Provides
     fun provideAuthRepository( //AuthRepository
         firebaseAuthDataSource: FirebaseAuthDataSource
     ): AuthRepository = AuthRepositoryImpl(firebaseAuthDataSource)
+
+    @Provides
+    fun provideTokenProvider(
+        firebaseAuth: FirebaseAuth
+    ): TokenProvider = TokenProvider(firebaseAuth)
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(
+        tokenProvider: TokenProvider
+    ): AuthInterceptor = AuthInterceptor(tokenProvider)
+
+    @Provides
+    @Singleton
+    fun provideOkHTTPBuilder(
+        authInterceptor: AuthInterceptor
+    ): OkHTTPBuilder = OkHTTPBuilder(authInterceptor)
 }
