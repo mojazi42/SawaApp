70ad1fb711f0de80efe380d01bf7062059e2a751
etharalrehaili <85852139+etharalrehaili@users.noreply.github.com>
2025-05-05 13:35:49 +0300

    fetch user's info (name+email) to the profile screen & make user navigate to profile when they already logged in (token)

--- app/src/main/java/com/example/sawaapplication/screens/profile/screens/Profile.kt
+++ app/src/main/java/com/example/sawaapplication/screens/profile/screens/Profile.kt
@@ -1,7 +1,6 @@
-package com.example.sawaapplication.screens.profile
+package com.example.sawaapplication.screens.profile.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -20,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,18 +29,25 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.example.sawaapplication.R
-import com.example.sawaapplication.ui.screenComponent.CustomTextField
+import com.example.sawaapplication.screens.profile.vm.ProfileViewModel
 
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(navController: NavController, profileViewModel: ProfileViewModel = hiltViewModel()) {
+    val userName by profileViewModel.userName.collectAsState()
+    val userEmail by profileViewModel.userEmail.collectAsState()
 
     var readOnly by remember { mutableStateOf(false) }
+    var editedName by remember { mutableStateOf(userName ?: "") }
+    var editedEmail by remember { mutableStateOf(userEmail ?: "") }
 
     Column(
         modifier = Modifier
@@ -76,16 +82,16 @@ fun ProfileScreen() {
         }
         //Name
         TextField(
-            value = "Testing Name",
-            onValueChange = {},
+            value = editedName,
+            onValueChange = { editedName = it },
             readOnly = readOnly,
             textStyle = TextStyle(
                 textAlign = TextAlign.Center,
                 fontWeight = FontWeight.Bold,
                 fontSize = 20.sp
             ),
-            modifier = Modifier.
-            wrapContentSize()
+            modifier = Modifier
+                .wrapContentSize()
                 .padding(integerResource(R.integer.zero).dp),
             colors = TextFieldDefaults.colors(
 
@@ -101,16 +107,33 @@ fun ProfileScreen() {
             )
         )
 
-        //Email
-        Text(
-           "TestingEmail@gmail.com",
-            fontSize = integerResource(R.integer.textSize1).sp,
-            color = Color.Gray
+        // Email TextField
+        TextField(
+            value = editedEmail,
+            onValueChange = { editedEmail = it },
+            readOnly = readOnly,
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = integerResource(R.integer.textSize1).sp
+            ),
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(integerResource(R.integer.zero).dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+            )
         )
+
         Spacer(Modifier.height(integerResource(R.integer.pioSpacer).dp))
 
-        //Pio
-        Text("About me:",
+        // About Me Section
+        Text(
+            text = stringResource(id = R.string.aboutMe),
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
             fontSize = integerResource(R.integer.textSize2).sp
@@ -119,8 +142,8 @@ fun ProfileScreen() {
             value = "Hi! I like to explore and try new things",
             onValueChange = {},
             readOnly = readOnly,
-            modifier = Modifier.
-            wrapContentSize()
+            modifier = Modifier
+                .wrapContentSize()
                 .padding(integerResource(R.integer.zero).dp),
             singleLine = false,
             textStyle = TextStyle(
