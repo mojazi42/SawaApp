2d9cfaeb64e74acf50ec66fe9b5f9d0c78ffd813
Ahad <ahadsaad45@gmail.com>
2025-05-11 00:51:09 +0300

    CA-131- Create Post Logic

--- app/src/main/java/com/example/sawaapplication/screens/post/presentation/screens/CreatePostScreen.kt
+++ app/src/main/java/com/example/sawaapplication/screens/post/presentation/screens/CreatePostScreen.kt
@@ -1,4 +1,4 @@
-package com.example.sawaapplication.screens.post.presentation.vmModels
+package com.example.sawaapplication.screens.post.presentation.screens
 
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
---
+++ app/src/main/java/com/example/sawaapplication/screens/post/presentation/vmModels/CreatePostVM.kt
@@ -0,0 +1,15 @@
+package com.example.sawaapplication.screens.post.presentation.vmModels
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CreatePostVM @Inject constructor(){
+
+    //image
+    //description
+    //date & time
+    //user
+    //community
+
+}
