cbf6a30fbbefab71942603cff006cf92b6c36337
Ahad <ahadsaad45@gmail.com>
2025-05-11 13:10:49 +0300

    CA-131- Create Post Logic

--- app/src/main/java/com/example/sawaapplication/screens/post/presentation/vmModels/CreatePostVM.kt
+++ app/src/main/java/com/example/sawaapplication/screens/post/presentation/vmModels/CreatePostVM.kt
@@ -2,14 +2,14 @@ package com.example.sawaapplication.screens.post.presentation.vmModels
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
-@HiltViewModel
-class CreatePostVM @Inject constructor(){
-
-    //image
-    //description
-    //date & time
-    //user
-    //community
-
-}
+//
+//@HiltViewModel
+//class CreatePostVM @Inject constructor(){
+//
+//    //image
+//    //description
+//    //date & time
+//    //user
+//    //community
+//
+//}
