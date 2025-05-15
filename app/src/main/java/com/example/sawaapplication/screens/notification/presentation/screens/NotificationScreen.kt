65ba3e33205c2997ffba88d4f16339dd4abd133c
Shahad Ali Aldawsari <shahodah.ali@gmail.com>
2025-05-15 13:51:06 +0300

    CA-155-Screens-dimensions-and-language NotificationScree is done

--- app/src/main/java/com/example/sawaapplication/screens/notification/presentation/screens/NotificationScreen.kt
+++ app/src/main/java/com/example/sawaapplication/screens/notification/presentation/screens/NotificationScreen.kt
@@ -1,28 +1,31 @@
 package com.example.sawaapplication.screens.notification.presentation.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
+import com.example.sawaapplication.R
 import com.example.sawaapplication.screens.notification.presentation.screens.component.NotificationCard
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.navigation.NavHostController
 import com.example.sawaapplication.screens.notification.presentation.viewmodels.NotificationViewModel
 import java.text.SimpleDateFormat
-import kotlin.OptIn
-import kotlin.String
 
 @SuppressLint("SimpleDateFormat")
 @Composable
 fun NotificationScreen(
-    navController: NavController,
+//    navController: NavController,
     notificationViewModel: NotificationViewModel = hiltViewModel(),
 ) {
     val notifications = notificationViewModel.notifications.collectAsState().value
@@ -31,24 +34,23 @@ fun NotificationScreen(
         notificationViewModel.markNotificationsAsSeen()
     }
 
-
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp), // or use your integerResource if needed
+            .padding(integerResource(R.integer.mediumSpace).dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(integerResource(R.integer.mediumSpace).dp))
 
         if (notifications.isEmpty()) {
-            Text(text = "No notifications.")
+            Text(text = stringResource(R.string.noNotifications))
         } else {
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 items(notifications.size) { index ->
                     val notification = notifications[index]
                     NotificationCard(
-                        name = "System Notification",
+                        name = stringResource(R.string.systemNotification),
                         time = SimpleDateFormat("hh:mm a").format(notification.timestamp),
                         action = notification.message,
                         profileImage = null,
