8a9ab4a181f5125fa3e1ba1e8c4c1f4bca560a57
Shahad Ali Aldawsari <shahodah.ali@gmail.com>
2025-05-19 08:59:09 +0300

    CA-186-Leave community dialog is added in the ExploreScreen

--- app/src/main/java/com/example/sawaapplication/screens/communities/presentation/screens/ExploreScreen.kt
+++ app/src/main/java/com/example/sawaapplication/screens/communities/presentation/screens/ExploreScreen.kt
@@ -24,12 +24,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.sawaapplication.R
 import com.example.sawaapplication.screens.communities.presentation.screens.component.ExploreCommunityCardList
 import com.example.sawaapplication.screens.communities.presentation.vmModels.CommunityFilterType
 import com.example.sawaapplication.screens.communities.presentation.vmModels.ExploreCommunityViewModel
+import com.example.sawaapplication.ui.screenComponent.CustomConfirmationDialog
 import com.example.sawaapplication.ui.screenComponent.SearchField
 
 @Composable
@@ -43,6 +46,9 @@ fun ExploreScreen(
 
     val listState = rememberLazyListState()
     var scrollToTopTrigger by remember { mutableStateOf(0) }
+
+    var showConfirmationDialog by remember { mutableStateOf(false) }
+    var selectedCommunityId by remember { mutableStateOf<String?>(null) }
 
     // Scroll to top when scrollToTopTrigger changes
     LaunchedEffect(scrollToTopTrigger) {
@@ -118,8 +124,25 @@ fun ExploreScreen(
                 viewModel.joinCommunity(communityId, viewModel.currentUserId)
             },
             onLeaveClick = { communityId ->
-                viewModel.leaveCommunity(communityId, viewModel.currentUserId)
+                selectedCommunityId = communityId
+                showConfirmationDialog = true
+//                viewModel.leaveCommunity(communityId, viewModel.currentUserId)
             }
         )
+
+        if(showConfirmationDialog && selectedCommunityId != null){
+            CustomConfirmationDialog(
+                message = stringResource(R.string.areYouSureCommunity),
+                onConfirm = {
+                    viewModel.leaveCommunity(selectedCommunityId!!, viewModel.currentUserId)
+                    showConfirmationDialog = false
+                    selectedCommunityId = null
+                },
+                onDismiss = {
+                    showConfirmationDialog = false
+                    selectedCommunityId = null
+                }
+            )
+        }
     }
 }
