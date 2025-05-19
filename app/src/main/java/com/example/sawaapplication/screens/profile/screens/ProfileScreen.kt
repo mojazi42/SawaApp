9b17325a70caf61360c7a5e1b5bcb0829cf5ea1f
RaghadAlmehmadi <160329803+RaghadAlmehmadi@users.noreply.github.com>
2025-05-19 11:05:51 +0300

    CA-185 fetch and display posts in user profile

--- app/src/main/java/com/example/sawaapplication/screens/profile/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/sawaapplication/screens/profile/screens/ProfileScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -22,8 +21,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -45,21 +46,39 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.sawaapplication.R
 import com.example.sawaapplication.navigation.Screen
 import com.example.sawaapplication.screens.authentication.presentation.vmModels.LogOutViewModel
+import com.example.sawaapplication.screens.home.presentation.screens.component.CustomTabRow
+import com.example.sawaapplication.screens.home.presentation.vmModels.HomeViewModel
 import com.example.sawaapplication.screens.profile.vm.ProfileViewModel
 
 @Composable
 fun ProfileScreen(
     navController: NavController,
-    profileViewModel: ProfileViewModel = hiltViewModel()
+    profileViewModel: ProfileViewModel = hiltViewModel(),
+    homeViewModel: HomeViewModel = hiltViewModel()
 ) {
     val userName by profileViewModel.userName.collectAsState()
     val userEmail by profileViewModel.userEmail.collectAsState()
     val aboutMe by profileViewModel.aboutMe.collectAsState()
     val imageUrl by profileViewModel.profileImageUrl.collectAsState()
+    val userCurrentId by profileViewModel.currentUserId.collectAsState()
 
     val logOutViewModel: LogOutViewModel = hiltViewModel()
     var showMenu by remember { mutableStateOf(false) }
 
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val tabs = listOf("Posts", "Likes")
+
+    LaunchedEffect(Unit) {
+        profileViewModel.loadCurrentUserId()
+    }
+    LaunchedEffect(selectedTabIndex, userCurrentId) {
+        if (userCurrentId.isNotEmpty()) {
+            when (selectedTabIndex) {
+                0 -> homeViewModel.fetchPostsByUser(userCurrentId)
+                1 -> homeViewModel.fetchLikedPostsByUser(userCurrentId)
+            }
+        }
+    }
     Box(modifier = Modifier.fillMaxSize()) {
         IconButton(
             onClick = { showMenu = !showMenu },
@@ -101,6 +120,7 @@ fun ProfileScreen(
             Box(
                 modifier = Modifier.size(integerResource(R.integer.photoBoxSize).dp)
             ) {
+
                 Image(
                     painter = if (imageUrl != null)
                         rememberAsyncImagePainter(imageUrl)
@@ -119,16 +139,16 @@ fun ProfileScreen(
                     modifier = Modifier
                         .align(Alignment.BottomEnd)
                         .background(
-                            color = Color(0xFFFF5722) ,
+                            color = Color(0xFFFF5722),
                             shape = RoundedCornerShape(48.dp)
                         )
-                        .size(32.dp)
+                        .size(24.dp)
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Create,
                         contentDescription = "Edit",
                         modifier = Modifier
-                            .size(24.dp),
+                            .size(16.dp),
                         tint = MaterialTheme.colorScheme.background
                     )
                 }
@@ -148,8 +168,7 @@ fun ProfileScreen(
                 textAlign = TextAlign.Center,
                 fontSize = integerResource(R.integer.textSize1).sp
             )
-
-            Spacer(Modifier.height(integerResource(R.integer.pioSpacer).dp))
+            Spacer(Modifier.height(8.dp))
 
             Text(
                 text = stringResource(id = R.string.aboutMe),
@@ -164,6 +183,20 @@ fun ProfileScreen(
                 fontSize = integerResource(R.integer.textSize2).sp,
                 modifier = Modifier.padding(horizontal = 16.dp)
             )
+                Spacer(Modifier.height(12.dp))
+
+                // Tab Row for "Posts" and "Likes"
+            CustomTabRow(
+                tabs = tabs,
+                selectedTabIndex = selectedTabIndex,
+                onTabSelected = { selectedTabIndex = it }
+            )
+                // Posts and Liked Posts
+                when (selectedTabIndex) {
+                    0 -> MyPostsTab(homeViewModel, navController, userCurrentId)
+                    1 -> PostsTabLike(homeViewModel, navController, userCurrentId)
+                }
+            }
         }
     }
-}
+
