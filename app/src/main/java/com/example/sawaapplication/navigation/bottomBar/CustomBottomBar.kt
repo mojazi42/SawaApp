31e191b6ddd8a1c451ac819cf07a9a13e2e9ecab
Shouq Albdrani <shouqalbdrani@gmail.com>
2025-05-25 10:52:05 +0300

    CA-236 translation bottom bar and profile tabs

--- app/src/main/java/com/example/sawaapplication/navigation/bottomBar/CustomBottomBar.kt
+++ app/src/main/java/com/example/sawaapplication/navigation/bottomBar/CustomBottomBar.kt
@@ -14,21 +14,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import com.example.sawaapplication.R
 
+data class NavigationItem(
+    val title: String,
+    val icon: ImageVector,
+)
 @Composable
 fun CustomBottomBar(
     selectedIndex: Int,
     onItemSelected: (Int) -> Unit
 ) {
+    // Localized item titles
+    val navigationItems = listOf(
+        NavigationItem(
+            title = stringResource(R.string.Home),
+            icon = Icons.Filled.Home,
+        ),
+        NavigationItem(
+            title = stringResource(R.string.Explore),
+            icon = Icons.Rounded.GridView,
+        ),
+        NavigationItem(
+            title = stringResource(R.string.Communities),
+            icon = Icons.Default.Group,
+        ),
+        NavigationItem(
+            title = stringResource(R.string.chats),
+            icon = Icons.Filled.ChatBubble,
+        )
+    )
+
     NavigationBar(
         containerColor = MaterialTheme.colorScheme.background
     ) {
         navigationItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selectedIndex == index,
-                onClick = {
-                    onItemSelected(index)
-                },
+                onClick = { onItemSelected(index) },
                 icon = {
                     Icon(
                         imageVector = item.icon,
@@ -39,39 +63,15 @@ fun CustomBottomBar(
                     Text(
                         text = item.title,
                         color = if (selectedIndex == index)
-                           MaterialTheme.colorScheme.primary
+                            MaterialTheme.colorScheme.primary
                         else Color.Gray
                     )
                 },
                 colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor =  MaterialTheme.colorScheme.background,
+                    selectedIconColor = MaterialTheme.colorScheme.background,
                     indicatorColor = MaterialTheme.colorScheme.primary
                 )
             )
         }
     }
 }
-
-data class NavigationItem(
-    val title: String,
-    val icon: ImageVector,
-)
-
-val navigationItems = listOf(
-    NavigationItem(
-        title = "Home",
-        icon = Icons.Filled.Home,
-    ),
-    NavigationItem(
-        title = "Explore",
-        icon = Icons.Rounded.GridView,
-    ),
-    NavigationItem (
-        title = "Communities",
-        icon = Icons.Default.Group,
-    ),
-    NavigationItem(
-        title = "Chats",
-        icon = Icons.Filled.ChatBubble,
-    )
-)
